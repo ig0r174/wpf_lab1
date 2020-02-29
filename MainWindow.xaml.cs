8396f5eba62f01d051cb765092cebf5c8e95bb3b
Igor <hstllbg@icloud.com>
2020-02-29 13:55:11 +0300

    Первоначальный проект

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox txtNumber = new TextBox();
+            txtNumber.TextChanged += new EventHandler(TextChanged);
+            AllNumbers.Children.Add(txtNumber);
+        }
+
+        private void TextChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
