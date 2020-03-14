0f64f8030a8709809c37e52d0ef34b712c7fe0b0
Igor <hstllbg@icloud.com>
2020-03-14 11:55:12 +0300

    Update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,9 +27,20 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            int number = AllNumbers.Children.Count;
             TextBox txtNumber = new TextBox();
             txtNumber.TextChanged += TextChanged;
             AllNumbers.Children.Add(txtNumber);
+
+            Button btn = new Button();
+            btn.Content = "Delete";
+            btn.Click += btn_Click;
+            AllButtons.Children.Add(btn);
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
 
         private void TextChanged(object sender, EventArgs e)
@@ -37,15 +48,23 @@ namespace WpfApp1
             double sum = 0;
             foreach (var item in AllNumbers.Children)
             {
-                if( item is TextBox )
+                if ( item is TextBox )
                 {
+                    double number = 0;
                     var tb = (TextBox)item;
-                    if (tb.Text.Length > 0)
-                        sum += double.Parse(tb.Text);
+                    if (double.TryParse(tb.Text, out number) && tb.Text.Length > 0 )
+                        sum += number;
+                    else MessageBox.Show("It is not a number");
                 }
             }
             ResultLabel.Content = "Результат: " + sum;
         }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            AllNumbers.Children.RemoveAt(AllNumbers.Children.Count - 1);
+            AllButtons.Children.RemoveAt(AllButtons.Children.Count - 1);
+           // TextChanged(false);
+        }
     }
 }
