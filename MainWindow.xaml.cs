b8dcb2452540e4287af21a6595bf10e4a4743c9d
Igor <hstllbg@icloud.com>
2020-03-21 12:44:34 +0300

    Final 21.03.2020

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -29,42 +29,104 @@ namespace WpfApp1
         {
             int number = AllNumbers.Children.Count;
             TextBox txtNumber = new TextBox();
-            txtNumber.TextChanged += TextChanged;
+            txtNumber.Height = 22;
+            txtNumber.Margin = new Thickness(0, 0, 0, 10);
+            txtNumber.TextChanged += DataUpdated;
+            txtNumber.Name = "tb" + AllNumbers.Children.Count;
             AllNumbers.Children.Add(txtNumber);
 
             Button btn = new Button();
             btn.Content = "Delete";
-            btn.Click += btn_Click;
+            btn.Click += Btn_Click;
+            btn.Height = 22;
+            btn.Margin = new Thickness(0, 0, 0, 10);
+            btn.Name = "btn" + AllButtons.Children.Count;
             AllButtons.Children.Add(btn);
+
+            ComboBox combobox = new ComboBox();
+
+            foreach (var action in new char[4] {'+', '-', '/', '*'})
+            {
+                combobox.Items.Add(action);
+            }
+
+            combobox.SelectedIndex = 0;
+            combobox.Height = 22;
+            combobox.Margin = new Thickness(0, 0, 0, 10);
+            combobox.Name = "cb" + AllComboBoxes.Children.Count;
+            combobox.SelectionChanged += DataUpdated;
+
+            AllComboBoxes.Children.Add(combobox);
         }
 
-        private void btn_Click(object sender, RoutedEventArgs e)
+        private void DataUpdated(object sender, SelectionChangedEventArgs e)
         {
-            
+            UpdateResult();
+        }
+
+        private void DataUpdated(object sender, TextChangedEventArgs e)
+        {
+            UpdateResult();
+        }
+
+        private void UpdateResult()
+        {
+            double result = 0;
+            decimal outDecimal;
+            foreach (var item in AllNumbers.Children)
+            {
+                if (item is TextBox)
+                {
+                    double number;
+                    var tb = (TextBox)item;
+                    double value = Convert.ToDouble(tb.Text);
+
+                    if (!decimal.TryParse(tb.Text, out outDecimal)) continue;
+
+                    int id = Convert.ToInt32(tb.Name.Substring(2));
+                    ComboBox cb = AllComboBoxes.Children[id] as ComboBox;
+
+                    switch (cb.SelectedItem)
+                    {
+                        case '*':
+                            result *= value;
+                            break;
+                        case '/':
+                            result /= value;
+                            break;
+                        case '+':
+                            result += value;
+                            break;
+                        case '-':
+                            result -= value;
+                            break;
+                    }
+                }
+            }
+            ResultLabel.Content = "Результат: " + result;
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            Button deleteButton = sender as Button;
+            int deleteId = Convert.ToInt32(deleteButton.Name.Substring(3));
+            AllButtons.Children.RemoveAt(deleteId);
+            AllComboBoxes.Children.RemoveAt(deleteId);
+            AllNumbers.Children.RemoveAt(deleteId);
+            UpdateResult();
         }
 
         private void TextChanged(object sender, EventArgs e)
         {
-            double sum = 0;
-            foreach (var item in AllNumbers.Children)
-            {
-                if ( item is TextBox )
-                {
-                    double number = 0;
-                    var tb = (TextBox)item;
-                    if (double.TryParse(tb.Text, out number) && tb.Text.Length > 0 )
-                        sum += number;
-                    else MessageBox.Show("It is not a number");
-                }
-            }
-            ResultLabel.Content = "Результат: " + sum;
+            UpdateResult();
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             AllNumbers.Children.RemoveAt(AllNumbers.Children.Count - 1);
             AllButtons.Children.RemoveAt(AllButtons.Children.Count - 1);
-           // TextChanged(false);
+            AllComboBoxes.Children.RemoveAt(AllComboBoxes.Children.Count - 1);
+            UpdateResult();
         }
     }
 }
